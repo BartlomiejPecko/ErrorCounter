99887ea20dca3802b6397510c52cfa90f0b0f0a4
bpecko <bartlomiejpecko@gmail.com>
2023-07-14 14:33:45 +0200

    Major code refactor

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,49 +1,59 @@
-
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.*;
 import java.util.*;
 
 public class Counter {
+    public static void countKeyOccurrences(String inputFilePath, String outputFilePath) {
+        Map<String, Integer> keyCounts = new HashMap<>(); //map storing count of keys
 
-    public static List<String> countErrors(String logFilePath) throws IOException, ParseException {
-        List<String> uniqueErrorList = new ArrayList<>();
-
-        List<String> XXX = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(logFilePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
             String line;
-            StringBuffer sb = new StringBuffer();
             while ((line = reader.readLine()) != null) {
-                if (line.length() > 0) {
-                    char firstChar = line.charAt(0);
-                    if (firstChar >= '0' && firstChar <= '9') {
-                        if (!sb.isEmpty()) {
-                            XXX.add(sb.toString());
-                            sb.setLength(0);
-                        }
-                    }
-                    sb.append(line);
-                }
-
-            }
-        }
-
-        List<LogItem> logItemList = new ArrayList<>();
-
-        for (String singleLog : XXX) {
-            LogItem logItem = new LogItem(singleLog);
-            logItemList.add(logItem);
-            for (String error : logItem.getErrors()) {
-                if (!uniqueErrorList.contains(error)) {
-                    uniqueErrorList.add(error);
+                String key = getKeyFromLine(line);
+                if (key != null) {
+                    keyCounts.put(key, keyCounts.getOrDefault(key, 0) + 1);
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return uniqueErrorList;
+        saveToTextFile(keyCounts, outputFilePath);
+        saveToJson(keyCounts, outputFilePath);
+    }
+
+    private static String getKeyFromLine(String line) {
+        String[] parts = line.split(" ");
+        for (String part : parts) {
+            if (part.startsWith("com.scc")) {
+                return part.replaceAll("[0-9]", "x");
+            }
+        }
+        return null;
+    }
+    private static void saveToTextFile(Map<String, Integer> keyCounts, String outputFilePath) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outputFilePath))) {
+            for (Map.Entry<String, Integer> entry : keyCounts.entrySet()) {
+                writer.println(entry.getKey() + " <-- repeated " + entry.getValue() + " times");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void saveToJson(Map<String, Integer> keyCounts, String outputFilePath) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject jsonObject = new JsonObject();
+        for (Map.Entry<String, Integer> entry : keyCounts.entrySet()) {
+            jsonObject.addProperty(entry.getKey(), entry.getValue());
+        }
+
+        try (Writer writer = new FileWriter(outputFilePath.replace(".txt", ".json"))) {
+            gson.toJson(jsonObject, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,17 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the path to log file");
-        String logFilePath = "GcmWebServices_Trace.txt";
 
-        long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis(); //starts timer
 
-        try {
-            List<String> uniqueErrors = Counter.countErrors(logFilePath);
-            int totalUniqueErrors = uniqueErrors.size();
+        String inputFilePath = "GcmWebServices_Trace.txt";
+        String outputFilePath = "keyCounts.txt";
+        Counter.countKeyOccurrences(inputFilePath, outputFilePath);
 
-            System.out.println("Total count of unique errors: " + totalUniqueErrors);
-            System.out.println("Unique errors:");
-
-            Map<String, Integer> errorCounts = new HashMap<>();
-            for (String error : uniqueErrors) {
-                errorCounts.put(error, errorCounts.getOrDefault(error, 0) + 1);
-            }
-
-            for (Map.Entry<String, Integer> entry : errorCounts.entrySet()) {
-                String error = entry.getKey();
-                int count = entry.getValue();
-                System.out.println(error + ": " + count + " times");
-            }
-
-            // Convert errorCounts map to JSON
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(errorCounts);
-
-            // Save JSON to a file
-            try (FileWriter writer = new FileWriter("error_counts.json")) {
-                writer.write(json);
-            }
-
-            System.out.println("Error counts saved to error_counts.json");
-        } catch (IOException | ParseException e) {
-            System.out.println("File not found");
-        }
-
-        long endTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis(); //ends timer
         long executionTime = endTime - startTime;
 
         System.out.println("Execution time: " + executionTime + " milliseconds");
+        System.out.println("Saved file to keyCounts.txt");
+
     }
 }
